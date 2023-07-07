1ba14d364b892d6d94e046fdebedb162e58fec1d
Harsh Agarwal <harsh821agarwal@gmail.com>
2023-07-07 21:37:29 +0530

    feat: add Tag and Button components

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button class="bg-[#5BADEC] text-white py-2 px-5 rounded-full w-[200px] h-[48px] text-base">
+      + Add New
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/app/components/Tag.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Tag = ({tagName}) => {
+  return (
+    <div className='flex justify-between px-6 py-6 w-[722px] ml-20 items-center border border-[#CECFD8] rounded-lg'>
+        <div className='flex gap-4 items-center'>
+          <Image src="/icons/tag.svg" alt='tag' width={40} height={40}/>
+          <div className='text-xl text-[#0B103A]'>{tagName}</div>
+        </div>
+        <Image src="/icons/arrow.svg" alt='arrow' width={40} height={40}/>
+    </div>
+  )
+}
+
+export default Tag
